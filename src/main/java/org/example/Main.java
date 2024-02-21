b7697b74a1bac132a81a19362301c83f31b157b7
Ale20p <alescode1@gmail.com>
2024-02-21 11:41:27 -0500

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // find the sum of each row, find the max number of each row
+//    int[][] number;
+//    number = new int[2][3];
+
+    // Array thing
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Please enter the number of integers you want: ");
+//        int numberOfInt = scanner.nextInt();
+//
+//        int[] numbers = new int[numberOfInt];
+    }
+}
